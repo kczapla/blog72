e4d4b69271da92e5c9449b9b5cac7c2014450c76
kczapla <k.czapla68@gmail.com>
2020-12-19 22:43:08 +0000

    blog item image

--- src/blog/postPreview.js
+++ src/blog/postPreview.js
@@ -2,9 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-import { PostPreviewAuthor } from './author'
-import { PostTitle, PostPreviewDescription } from './post'
-import { Tags } from './tag'
+// import { PostPreviewAuthor } from './author'
+// import { PostTitle, PostPreviewDescription } from './post'
+// import { Tags } from './tag'
 
 export const PostPreviewDate = ({ date }) => {
   return (
@@ -16,47 +16,42 @@ PostPreviewDate.propTypes = {
   date: PropTypes.string
 }
 
-const PostGridLayout = styled.div`
-  display: grid;
-  grid-auto-columns: 1fr 3fr;
-  grid-auto-rows: 1fr min-content;
-  grid-template-areas:
-    "author tags pictureItem"
-    "main main pictureItem"
-    "main main pictureItem"
+const BlogItem = styled.article`
+  display: flex
 `
 
-const Author = styled.div`
-  grid-area: author;
+const BlogItemImageContainer = styled.div`
+  postition: relative;
+  overflow: hidden;
 `
-
-const Taggs = styled.div`
-  grid-area: tags;
-`
-
-const Main = styled.div`
-  grid-area: main;
-`
-
-const PictureItem = styled.div`
-  grid-area: pictureItem;
+const BlogItemImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center center;
 `
 
 const PostPreview = ({ post: { user: { name, image, userLink }, tags, created, title, content } }) => {
+  console.log(name, image, userLink, tags, created, title, content)
   return (
-    <PostGridLayout>
-      <Author>
-        <PostPreviewAuthor name={name} image={image} userLink={userLink}></PostPreviewAuthor>
-      </Author>
-      <Taggs><Tags tags={tags}/></Taggs>
-      <Main>
-        <PostTitle>{title}</PostTitle>
-        <PostPreviewDescription text={content} charLimit={200}/>
-      </Main>
-      <PictureItem>
-        <img object-fit="cover" src="https://www.obrazydeco.pl/1007-large_default/jan-pawel-ii-obraz-drukowany-na-plotnie.jpg" width={250} height={250}/>
-      </PictureItem>
-    </PostGridLayout>
+    <BlogItem>
+      <BlogItemImageContainer>
+        <BlogItemImage src="https://www.obrazydeco.pl/1007-large_default/jan-pawel-ii-obraz-drukowany-na-plotnie.jpg"/>
+      </BlogItemImageContainer>
+    </BlogItem>
+    // <PostGridLayout>
+    //   <Author>
+    //     <PostPreviewAuthor name={name} image={image} userLink={userLink}></PostPreviewAuthor>
+    //   </Author>
+    //   <Taggs><Tags tags={tags}/></Taggs>
+    //   <Main>
+    //     <PostTitle>{title}</PostTitle>
+    //     <PostPreviewDescription text={content} charLimit={200}/>
+    //   </Main>
+    //   <PictureItem>
+    //     <img object-fit="cover" src="https://www.obrazydeco.pl/1007-large_default/jan-pawel-ii-obraz-drukowany-na-plotnie.jpg" width={250} height={250}/>
+    //   </PictureItem>
+    // </PostGridLayout>
   )
 }
 
