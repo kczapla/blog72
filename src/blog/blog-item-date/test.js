98760451386c8abd274f82ff118bcbe9deca0b55
kczapla <k.czapla68@gmail.com>
2020-12-22 18:54:29 +0000

    add blog item date tests

--- src/blog/blog-item-date/test.js
+++ src/blog/blog-item-date/test.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 
+import '@testing-library/jest-dom/extend-expect'
+
+import { render, screen } from '@testing-library/react'
+
 import BlogItemDate from './index'
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<BlogItemDate date={'2020-01-01'}/>)
-    .toJSON()
-  expect(tree).toMatchSnapshot()
+describe('blog item date', () => {
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(<BlogItemDate date={'2020-01-01'}/>)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('has format MMM DD, YYYY', () => {
+    render(<BlogItemDate date={'2020-01-01'}/>)
+    expect(screen.getByText('Jan 01, 2020')).toBeInTheDocument()
+  })
 })
