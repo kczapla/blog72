341baf218e7d2c88b246d091206839d40ff1f927
kczapla <k.czapla68@gmail.com>
2020-12-22 20:49:46 +0000

    add by line component

---
+++ src/blog/by-line/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { StyledByLine } from './style'
+
+const ByLine = () => {
+  return <StyledByLine> by </StyledByLine>
+}
+
+export default ByLine
---
+++ src/blog/by-line/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const StyledByLine = styled.span`
+  font-size: inherit;
+  font-style: inherit;
+  text-transform: lowercase;
+`
