53a4213090cfa30f9e0b8b595f62ddfcc1bd488e
kczapla <k.czapla68@gmail.com>
2020-12-23 14:26:26 +0000

    replace blog item content header

--- src/blog/postPreview.js
+++ src/blog/postPreview.js
@@ -2,13 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-import ByLine from './by-line'
 import BlogItemImage from './blog-item-image'
-import BlogItemTitle from './blog-item-title'
-import BlogItemAuthor from './blog-item-author'
 import BlogItemContent from './blog-item-content'
-import BlogItemPostDate from './blog-item-date'
-import BlogItemMetadataContainer from './blog-item-metadata-container'
+import BlogItemContentHeader from './blog-item-content-header'
 
 const BlogItemContainer = styled.article`
   display: flex;
@@ -34,12 +30,6 @@ const BlogItemContentContainter = styled.div`
   flex-direction: column;
 `
 
-const BlogItemContentHeader = styled.header`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 10px;
-`
-
 const PostPreview = ({ post: { author, image, created, title, content } }) => {
   return (
     <BlogItemContainer>
@@ -47,14 +37,7 @@ const PostPreview = ({ post: { author, image, created, title, content } }) => {
         <BlogItemImage image={image}/>
       </BlogItemImageContainer>
       <BlogItemContentContainter>
-        <BlogItemContentHeader>
-          <BlogItemTitle title={title}/>
-          <BlogItemMetadataContainer>
-            <BlogItemPostDate date={created}/>
-            <ByLine/>
-            <BlogItemAuthor href={author.href} name={author.name}/>
-          </BlogItemMetadataContainer>
-        </BlogItemContentHeader>
+        <BlogItemContentHeader title={title} created={created} author={author}/>
         <BlogItemContent href={'https://google.com'} content={content}/>
       </BlogItemContentContainter>
     </BlogItemContainer>
