0cbcf06a5d25add66af749f6d3a950e970573aa7
kczapla <k.czapla68@gmail.com>
2020-12-27 16:26:04 +0000

    update blog item style

--- src/components/blog-item/style.js
+++ src/components/blog-item/style.js
@@ -2,7 +2,9 @@ import styled from 'styled-components'
 
 export const StyledBlogItem = styled.div`
   display: flex;
+  width: 60%;
   flex-direction: row;
+  margin: 15px;
 
   &:hover {
     box-shadow: 0px 0px 30px #888888;
