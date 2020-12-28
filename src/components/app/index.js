d118a4e48d06f7c39e81e7f759fae7559a1b830d
kczapla <k.czapla68@gmail.com>
2020-12-28 17:31:43 +0000

    add navigation bar to app

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import Blog from '../blog'
 import BlogItemList from '../blog-item-list'
+import NavigationBar from './navigation-bar'
 
 export const App = () => {
-  return <Blog><BlogItemList/></Blog>
+  return (
+    <Router>
+      <NavigationBar/>
+      <Switch>
+        <Route exact={true} path='/blog'>
+          <Blog><BlogItemList/></Blog>
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
