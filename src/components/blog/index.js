30f40214814e33939228da8d830271259c29e8fc
kczapla <k.czapla68@gmail.com>
2021-01-04 21:10:06 +0000

    Remove maring from blog styleIt is set by main-content component in the app# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch add-component-for-domain-content# Changes to be committed:#	modified:   src/components/blog/index.js## Untracked files:#	src/domain/post/post-content/__snapshots__/#

--- src/components/blog/index.js
+++ src/components/blog/index.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 const Blog = styled.main`
     display: flex;
     justify-content: center;
-    margin: 30px 0 0 0;
     height: 100%;
 `
 export default Blog
