238260278773d4d6777cddcf96e7aa02467cb54f
kczapla <k.czapla68@gmail.com>
2021-01-06 20:55:21 +0000

    add math support

--- src/domain/post/post-content/index.js
+++ src/domain/post/post-content/index.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types'
 import ReactMarkdown from 'react-markdown'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { monoBlue as highlightstyle } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import math from 'remark-math'
+import TeX from '@matejmazur/react-katex'
+import 'katex/dist/katex.min.css'
 
 const CodeRenderer = ({ language, value }) => {
   return (
@@ -17,9 +20,21 @@ CodeRenderer.propTypes = {
   value: PropTypes.string
 }
 
+const InlineMathRenderer = ({ value }) => <TeX math={value}/>
+
+InlineMathRenderer.propTypes = {
+  value: PropTypes.string
+}
+
+const BlockMathRenderer = ({ value }) => <TeX block math={value}/>
+
+BlockMathRenderer.propTypes = {
+  value: PropTypes.string
+}
+
 const PostContent = ({ post: { content } }) => {
-  const renderers = { code: CodeRenderer }
-  return <ReactMarkdown renderers={renderers}>{content}</ReactMarkdown>
+  const renderers = { code: CodeRenderer, inlineMath: InlineMathRenderer, math: BlockMathRenderer }
+  return <ReactMarkdown plugins={[math]} renderers={renderers}>{content}</ReactMarkdown>
 }
 
 PostContent.propTypes = {
