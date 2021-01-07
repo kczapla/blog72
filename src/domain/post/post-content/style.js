43b5c8ee0bdb977d0a3bd36685a0647a0781834b
kczapla <k.czapla68@gmail.com>
2021-01-07 20:01:25 +0000

    Shrink images size to fit the box

--- src/domain/post/post-content/style.js
+++ src/domain/post/post-content/style.js
@@ -1,5 +1,18 @@
 import styled from 'styled-components'
+import ReactMarkdown from 'react-markdown'
 
 export const StyledPostContent = styled.article`
     font-color: black;
+`
+
+export const StyledReactMarkdown = styled(ReactMarkdown)`
+    & > h1, h2, h3, h4, h5, h6 {
+        text-align: center;
+    }
+    * > img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center center;
+    }
 `
