7b5aadf9d7c5fc832f873725ab73364b1f626507
kczapla <k.czapla68@gmail.com>
2021-01-11 22:01:10 +0000

    add loading spinner

---
+++ src/components/loading-spinner/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { StyledLoadingSpinner } from './style'
+
+const LoadingSpinner = () => {
+  return (
+    <StyledLoadingSpinner>
+      <div/><div/><div/><div/><div/><div/><div/><div/>
+    </StyledLoadingSpinner>
+  )
+}
+
+export default LoadingSpinner
