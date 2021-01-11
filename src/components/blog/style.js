0cd42ea3215c1652e822b7e56dbeac02cc1df9d9
kczapla <k.czapla68@gmail.com>
2021-01-11 20:46:25 +0000

    set max blog width

--- src/components/blog/style.js
+++ src/components/blog/style.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
-export const StyledBlog = styled.article`
+export const StyledBlog = styled.section`
     display: flex;
     justify-content: center;
     height: 100%;
+    max-width: 800px;
 `
