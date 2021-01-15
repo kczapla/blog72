f809f4cc1d23a23c9c141b2f08c5868512774897
kczapla <k.czapla68@gmail.com>
2021-01-15 22:35:39 +0000

    use function from test utils function

--- src/domain/post/test.js
+++ src/domain/post/test.js
@@ -1,22 +1,10 @@
 import React from 'react'
 import axios from 'axios'
 
-import { MemoryRouter } from 'react-router'
-
-import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react'
+import { screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
-import Post from './index'
-
-import Routes from '../../components/routes'
-
-const renderWithRouter = () => {
-  render(
-    <MemoryRouter initialEntries={['/posts/1']}>
-      <Routes/>
-    </MemoryRouter>
-  )
-}
+import { renderRouterWithInitialEntries } from '../../services/test-utils'
 
 jest.mock('./post-content', () => {
   return {
@@ -42,23 +30,23 @@ jest.useFakeTimers()
 describe('post', () => {
   it('uses fetched data from api', async () => {
     axios.get.mockResolvedValue({ data: { title: 'test title' } })
-    renderWithRouter(<Post/>)
+    renderRouterWithInitialEntries({ initialEntries: ['/posts/1'] })
     await expect(screen.findByText('test title')).resolves.toBeTruthy()
   })
   it('does not render when fetched data is empty', async () => {
     axios.get.mockRejectedValue('error')
-    renderWithRouter(<Post/>)
+    renderRouterWithInitialEntries({ initialEntries: ['/posts/1'] })
     await expect(screen.findByText('page not found')).resolves.toBeTruthy()
   })
   it('renders loading when fetching data', async () => {
     axios.get.mockResolvedValue({ data: { title: 'test title' } })
-    renderWithRouter(<Post/>)
+    renderRouterWithInitialEntries({ initialEntries: ['/posts/1'] })
     await waitForElementToBeRemoved(() => screen.queryByText('loading'))
   })
   it('fetches post with an id from uri', async () => {
     const mockedAxiosGet = jest.fn().mockRejectedValue('error')
     axios.get = mockedAxiosGet
-    renderWithRouter(<Post/>)
+    renderRouterWithInitialEntries({ initialEntries: ['/posts/1'] })
     await waitFor(() => { expect(mockedAxiosGet.mock.calls[0][0]).toBe('/posts/1') })
   })
 })
