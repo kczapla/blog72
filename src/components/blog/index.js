25feebf747b615cec0e51c96bf6087370bbcdead
kczapla <k.czapla68@gmail.com>
2021-01-19 23:02:52 +0000

    add loading spinner when fetching blog posts

--- src/components/blog/index.js
+++ src/components/blog/index.js
@@ -4,9 +4,14 @@ import { StyledBlog } from './style'
 import { useFetchPosts } from './hook'
 
 import BlogItemList from '../blog-item-list'
+import LoadingSpinner from '../loading-spinner'
 
 const Blog = () => {
-  const posts = useFetchPosts()
+  const { posts, areLoading } = useFetchPosts()
+
+  if (areLoading) {
+    return <LoadingSpinner/>
+  }
 
   return (
     <StyledBlog>
